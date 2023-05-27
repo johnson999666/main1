af164d011ff13fe5b1442e39e1a599d95455beba
johnson999666 <123405062+johnson999666@users.noreply.github.com>
2023-05-27 09:59:33 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+const io = require('socket.io')(server);
+
+
+
+
+app.get('/', (req, res) => {
+  res.render('index', { name: 'World' });
+});
+
+
+
+
+
+
+
+app.listen(process.env.PORT ||3000, "0.0.0.0", () => {
+  console.log('Server listening on port 3000');
+});
