e2c00782ef0734dec372d1d1746d1c448698e178
johnson999666 <123405062+johnson999666@users.noreply.github.com>
2023-06-06 13:04:08 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -32,16 +32,57 @@ app.get('/buy', (req, res) => {
 
 
 // Render the index page
+let cartCount = 0;
+let checkoutSubmitted = false; // Variable to track if the checkout form has been submitted
+
+let productCatalog = [
+  { id: 1, name: 'Shirt', price: '$10', imageSrc: '/images/green.jpg', title: 'Green Shirt' },
+  { id: 2, name: 'Trousers', price: '$20', imageSrc: '/images/blue.jpg', title: 'Blue Trousers' },
+  { id: 3, name: 'Dress', price: '$30', imageSrc: '/images/red.jpg', title: 'Red Dress' },
+  { id: 4, name: 'Hat', price: '$5', imageSrc: '/images/shirt.jpg', title: 'Black Hat' },
+  { id: 3, name: 'Dress', price: '$30', imageSrc: '/images/red.jpg', title: 'Red Dress' },
+  { id: 4, name: 'Hat', price: '$5', imageSrc: '/images/shirt.jpg', title: 'Black Hat' },
+  // Add more products here if needed
+];
+
+let selectedProducts = []; // Array to store the selected products
+
 app.get('/store', (req, res) => {
-  res.render('store', { buttons });
+  res.render('store', { cartCount, productCatalog, selectedProducts, checkoutSubmitted });
 });
 
-// Handle the like button POST request
-app.post('/store/like/:buttonIndex', (req, res) => {
-  const buttonIndex = parseInt(req.params.buttonIndex);
-  buttons[buttonIndex].likes++;
+app.post('/addToCart', (req, res) => {
+  const productId = req.body.productId;
+  const selectedProduct = productCatalog.find(product => product.id === parseInt(productId));
+
+  if (selectedProduct) {
+    cartCount++;
+    selectedProducts.push(selectedProduct); // Add the selected product to the selectedProducts array
+  } else {
+    res.status(404).send('Product not found.');
+  }
   res.redirect('/store');
 });
+
+app.post('/checkout', (req, res) => {
+  // Perform the necessary actions for checkout (e.g., process payment, place order, etc.)
+  // You can clear the cartCount or perform any other required operations here
+  
+  checkoutSubmitted = true; // Set checkoutSubmitted to true
+  res.redirect('/store');
+});
+
+app.post('/reset', (req, res) => {
+  // Reset the page to the original state
+  cartCount = 0;
+  selectedProducts = [];
+  checkoutSubmitted = false;
+  res.redirect('/store');
+});
+
+
+// Handle the like button POST request
+
 
 
 
@@ -76,7 +117,7 @@ app.post('/news', async (req, res) => {
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 const host = '0.0.0.0';
 app.listen(port, host, () => {
   console.log(`Server listening on port ${port}`);
